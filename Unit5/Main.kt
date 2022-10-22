6b96a5b8af8a77ad047be7845fbf1c2472085c9c
Tsubasa <realmadrid411tsubasa@gmail.com>
2022-10-22 21:37:25 +0900

    add unit5

--- Unit5/Main.kt
+++ Unit5/Main.kt
@@ -1,3 +1,9 @@
 fun main() {    
-    var singleton: Singleton = Singleton
+    var singleton1: Singleton = Singleton
+    var singleton2: Singleton = Singleton
+    if (singleton1 == singleton2) {
+        println("同じです")
+    } else {
+        println("違います")
+    }
 }
